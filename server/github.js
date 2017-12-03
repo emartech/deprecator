9d567140c0b513d93cf8615943e23bc811a4483f
Andras Csibi <andras.csibi@gmail.com>
2017-12-04 00:35:13 +0100

    pick stuff

--- server/github.js
+++ server/github.js
@@ -1,20 +1,27 @@
 const axios = require('axios');
+const _ = require('underscore');
 
 const config = require('./config');
 
+const instance = axios.create({
+  method: 'get',
+  baseURL: 'https://api.github.com',
+  headers: {Accept: 'application/vnd.github.cloak-preview'},
+  auth: {
+    username: config.githubUser,
+    password: config.githubToken,
+  }
+});
+
 const deprecations = async (ctx, next) => {
-  console.log(config)
-  let instance = axios.create({
-    method: 'get',
-    baseURL: 'https://api.github.com',
-    headers: {Accept: 'application/vnd.github.cloak-preview'},
-    auth: {
-      username: config.githubUser,
-      password: config.githubToken,
-    }
-  });
-  let response = await instance.get('/search/commits?q=org:emartech+deprecator&order=desc&sort=committer-date');
-  ctx.body = response.data;
+  const response = await instance.get('/search/commits?q=org:emartech+deprecate&order=desc&sort=author-date');
+
+  ctx.body = _.head(response.data.items, 10).map(c => ({
+    url: c.url,
+    date: c.commit.author.date,
+    msg: c.commit.message,
+    author: _.pick(c.author, 'login', 'avatar_url'),
+  }));
   ctx.response.status = 200;
 };
 
